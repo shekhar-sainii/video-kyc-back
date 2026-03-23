3caba27190a380e3d816a2faca2b308e7aedcf60
qdeveloper <shekhar@qservicesit.net>
2026-03-23 17:37:43 +0530

    Send Email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false, // Use STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
