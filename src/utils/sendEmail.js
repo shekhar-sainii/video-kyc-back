52b33f7de0349d209fbe5c39116dc0ea2db0f78c
qdeveloper <shekhar@qservicesit.net>
2026-03-23 17:29:36 +0530

    Send Email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,11 +2,17 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  family: 4, // Force IPv4
 });
 
 const sendEmail = async ({ to, subject, html }) => {
